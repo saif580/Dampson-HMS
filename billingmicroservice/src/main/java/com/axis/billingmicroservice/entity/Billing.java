26cb0f0e80aad68b4100f713616dbb7eaeb32dc5
vinayasonarkar <168065353+vinayasonarkar@users.noreply.github.com>
2024-06-08 00:03:46 +0530

    Add files via upload

---
+++ billingmicroservice/src/main/java/com/axis/billingmicroservice/entity/Billing.java
@@ -0,0 +1,90 @@
+package com.axis.billingmicroservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Billing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long billingId;
+
+    private Double amount;
+
+    private String status;
+
+    private LocalDateTime paymentDate;
+
+    private String paymentMethod;
+
+//    @Column(name = "user_id")
+//    private Long userId;
+
+    @Column(name = "patient_id")
+    private Long patientId;
+    
+	public Long getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(Long patientId) {
+		this.patientId = patientId;
+	}
+
+	public Long getBillingId() {
+		return billingId;
+	}
+
+	public void setBillingId(Long billingId) {
+		this.billingId = billingId;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(LocalDateTime paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+//	public Long getUserId() {
+//		return userId;
+//	}
+//
+//	public void setUserId(Long userId) {
+//		this.userId = userId;
+//	}
+
+    // getters and setters
+    
+}
