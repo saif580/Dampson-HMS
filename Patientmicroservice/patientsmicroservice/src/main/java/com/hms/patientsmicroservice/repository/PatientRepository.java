164548ccd9fee8087727aadd39bc86a625b4d483
Shreyash Sonawane <76249199+shreyashsonawane@users.noreply.github.com>
2024-06-13 11:45:58 +0530

    Delete Patientmicroservice directory

--- Patientmicroservice/patientsmicroservice/src/main/java/com/hms/patientsmicroservice/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hms.patientsmicroservice.repository;
-
-import com.hms.patientsmicroservice.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-    Patient findByEmail(String email);
-}
