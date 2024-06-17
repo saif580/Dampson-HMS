a5eb99718f5e09fdd8ce63a58eb6a202b907585a
vinayasonarkar <168065353+vinayasonarkar@users.noreply.github.com>
2024-06-17 23:44:48 +0530

    Delete billingmicroservice directory

--- billingmicroservice/src/main/java/com/axis/billingmicroservice/exception/InvalidPaymentMethodException.java
+++
@@ -1,7 +0,0 @@
-package com.axis.billingmicroservice.exception;
-
-public class InvalidPaymentMethodException extends RuntimeException {
-    public InvalidPaymentMethodException(String paymentMethod) {
-        super("Invalid payment method: " + paymentMethod + ". Valid methods are: Online, Cash, Prepaid.");
-    }
-}
