72246fd76a8beefb774e6ed0844553ca665d8d4b
vinayasonarkar <168065353+vinayasonarkar@users.noreply.github.com>
2024-06-18 00:37:57 +0530

    .

--- billingmicroservice/exception/NoAppointmentException.java
+++
@@ -1,7 +0,0 @@
-package com.axis.billingmicroservice.exception;
-
-public class NoAppointmentException extends RuntimeException {
-	public NoAppointmentException(Long patientId) {
-        super("Patient with ID " + patientId + " does not have an appointment.");
-    }
-}
